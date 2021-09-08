53e08e2965380355912ab9c17b71226391f84dca
Sergey Vorobyev <syvorobyev@gmail.com>
2021-09-08 18:48:31 +0300

    logos on the map

---
+++ mapLogos.js
@@ -0,0 +1,7 @@
+const logos = {
+    tokio: 'https://static.tildacdn.com/tild6266-3130-4335-b763-313836646239/tokio-logo.png',
+    mayak: 'https://static.tildacdn.com/tild6263-3731-4563-a630-366565303337/mayak-logo.png',
+    mac: 'https://static.tildacdn.com/tild6533-6230-4966-b932-663631656138/mac-logo.png',
+    kari: 'https://static.tildacdn.com/tild3737-6463-4534-a362-623866336137/kari-logo.png',
+    familia: 'https://static.tildacdn.com/tild6334-3135-4133-b937-623165343263/familia-logo.png'
+};
